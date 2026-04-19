f3882b1b453567110335850437641dd080f4ad46
MikeTryArtd <mikeleano26@gmail.com>
2026-04-19 18:22:13 +0800

    get all anime info

--- AnimeApp/src/Services/API.ts
+++ AnimeApp/src/Services/API.ts
@@ -1,15 +1,39 @@
 export async function getAnimeInfo(idOrSlug: string) {
-  const res = await fetch(`https://anipub.xyz/api/info/${idOrSlug}`);
+  
+  try{
+    const res = await fetch(`https://anipub.xyz/api/info/${idOrSlug}`);
 
-  if (!res.ok) throw new Error(`${res.status}`);
+    if (!res.ok) throw new Error(`${res.status}`);
+  
+    const data = await res.json();
+  
+    const fix = (p: string) =>
+      p?.startsWith("http") ? p : `https://anipub.xyz/${p}`;
+  
+    data.ImagePath = fix(data.ImagePath);
+    data.Cover = fix(data.Cover);
+  
+    return data;
+  } catch (error){
+    console.error('Error fetching anime info', error);
+  }
+}
 
-  const data = await res.json();
 
-  const fix = (p: string) =>
-    p?.startsWith("http") ? p : `https://anipub.xyz/${p}`;
-
-  data.ImagePath = fix(data.ImagePath);
-  data.Cover = fix(data.Cover);
-
-  return data;
+export async function getAllAnime(){
+    
+     try{
+      const url = `https://anipub.xyz/api/getAll`;
+      const res = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      });
+      if (!res.ok) throw new Error(`${res.status}`);
+      const data = await res.json();
+      return data;
+     } catch (error){
+      console.error("Error fetching aall anime", error);
+     }
 }
