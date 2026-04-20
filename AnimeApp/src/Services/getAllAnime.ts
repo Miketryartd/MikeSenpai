db7d4ec9221cac5604c89ceee33e6083a87333a4
MikeTryArtd <mikeleano26@gmail.com>
2026-04-20 10:03:23 +0800

    updated service

---
+++ AnimeApp/src/Services/getAllAnime.ts
@@ -0,0 +1,18 @@
+
+export async function getAllAnime(){
+    
+    try{
+     const url = `http://localhost:3000/mikesenpai/api/animeAll`;
+     const res = await fetch(url, {
+       method: "GET",
+       headers: {
+         "Content-Type": "application/json"
+       },
+     });
+     if (!res.ok) throw new Error(`${res.status}`);
+     const data = await res.json();
+     return data;
+    } catch (error){
+     console.error("Error fetching aall anime", error);
+    }
+}
