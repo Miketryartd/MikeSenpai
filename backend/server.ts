ea3051bf7825857777c5529bd7f53ccb8e55ad1b
MikeTryArtd <mikeleano26@gmail.com>
2026-04-20 10:02:38 +0800

    instead of using frontend for fetch used backend instead

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,13 @@
 require('dotenv').config();
 import express from "express";
 import cors from "cors";
-
+import animeRoutes from "./Routes/Anime";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+app.use('/mikesenpai', animeRoutes);
 
 const PORT = process.env.HOST || 3000;
 
