aae13e11d3a1dd3b6be5c02f96f3dad6869f3d61
MikeTryArtd <mikeleano26@gmail.com>
2026-04-20 10:34:26 +0800

    added route for search controller

--- backend/Routes/Anime.ts
+++ backend/Routes/Anime.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAllAnime } from "../Controllers/AllAnime";
-
+import { getAllAnime } from "../Controllers/AllAnimeController";
+import { SearchAnime } from "../Controllers/SearchAnimeController";
 const router = express.Router();
 
 
 router.get('/api/animeAll', getAllAnime);
-
+router.get('/api/searchAnime/:query', SearchAnime);
 export default router;
