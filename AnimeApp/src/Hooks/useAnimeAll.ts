5406046ef139877244625e38788d3ecb7c3bf892
MikeTryArtd <mikeleano26@gmail.com>
2026-04-20 10:04:15 +0800

    use own backend route instead of fetching from anipub

---
+++ AnimeApp/src/Hooks/useAnimeAll.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { getAllAnime } from "../Services/getAllAnime";
+
+export function useAnimeAll() {
+  const [anime, setAnime] = useState<number>(Number);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data: number = await getAllAnime(); 
+        console.log("API RES", data);
+        setAnime(data);
+      } catch (err: unknown) {
+        console.error("Error fetching all anime", err);
+
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("Something went wrong");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { anime, loading, error };
+}
