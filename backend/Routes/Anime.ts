8027378b4cf047471435f0040d4c426345d98d2a
MikeTryArtd <mikeleano26@gmail.com>
2026-04-22 21:07:31 +0800

    updated route

--- backend/Routes/Anime.ts
+++ backend/Routes/Anime.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { getAllAnime } from "../Controllers/AllAnimeController";
 import { SearchAnime } from "../Controllers/SearchAnimeController";
+import {TopRatedAnime} from "../Controllers/TopRatedAnimeController";
 const router = express.Router();
 
 
 router.get('/api/animeAll', getAllAnime);
 router.get('/api/searchAnime/:query', SearchAnime);
+router.get('/api/topRated', TopRatedAnime );
 export default router;
