f94693cba311af3bf927d123758a8c76255bcad7
MikeTryArtd <mikeleano26@gmail.com>
2026-04-22 21:06:33 +0800

    updated main style

--- AnimeApp/src/Pages/Main.tsx
+++ AnimeApp/src/Pages/Main.tsx
@@ -1,4 +1,6 @@
 
+import SearchAnime from "../Components/SearchAnime";
+import TopRatedShowcase from "../Components/TopRatedShowcase";
 import { useAnimeAll } from "../Hooks/useAnimeAll";
 
 
@@ -9,9 +11,16 @@ function Main (){
 
     return (
         <>
+        <div className="bg-[#0d0d14] text-purple-500 w-full h-screen">
+        <SearchAnime/>
+
+          <div>
+            <TopRatedShowcase/>
+          </div>
+
         <p>main</p>
          <h1>Total anime: {anime}</h1>
-   
+    </div>
         </>
     )
 }
