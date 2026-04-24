3869e2aa8b1cd91119d5a71ee91a74aefcfd613e
MikeTryArtd <mikeleano26@gmail.com>
2026-04-24 17:23:16 +0800

    updated main

--- AnimeApp/src/main.tsx
+++ AnimeApp/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
+import Home from './Pages/Home.tsx';
+import Main from './Pages/Main.tsx';
+import Detail from './Pages/Detail.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}></Route>
+        <Route path='/Home' element={<Home/>}></Route>
+        <Route path='/Main' element={<Main/>}></Route>
+        <Route path='/Mike-Senpai/:anime'></Route>
+        <Route path='/Detail/:id/:finder' element={<Detail/>}></Route>
+      </Routes>
+   </BrowserRouter>
   </StrictMode>,
 )
