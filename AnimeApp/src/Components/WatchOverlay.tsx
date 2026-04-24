0d37dce5464142971e519f65d2db168a7437b531
MikeTryArtd <mikeleano26@gmail.com>
2026-04-24 17:18:17 +0800

    makes button direct to detail

--- AnimeApp/src/Components/WatchOverlay.tsx
+++ AnimeApp/src/Components/WatchOverlay.tsx
@@ -1,26 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import type { WatchOverlayProps } from "../Types/Interface";
 
-
-function WatchOverlay({ children, onClick }: WatchOverlayProps) {
-    return (
-      <div className="relative group cursor-pointer">
-  
-  
-        {children}
-  
+function WatchOverlay({ children, id, finder }: WatchOverlayProps) {
+  const navigate = useNavigate();
+ 
+  const handleClick = () => {
     
-        <div className="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-  
-          <button
-            onClick={onClick}
-            className="cursor-pointer bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-lg font-semibold"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-player-play"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" /></svg>
-          </button>
-  
-        </div>
+   
+    navigate(`/Detail/${id}/${finder}`);
+  };
+
+  return (
+    <div className="relative group cursor-pointer">
+      {children}
+
+      <div className="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+        <button
+          onClick={handleClick}
+          className="cursor-pointer bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-lg font-semibold"
+        >
+           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-player-play"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" /></svg>
+        </button>
       </div>
-    );
-  }
-  
-  export default WatchOverlay;
+    </div>
+  );
+}
+
+export default WatchOverlay;
