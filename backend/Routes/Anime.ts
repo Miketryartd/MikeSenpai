334fb8b406b69c7913cf117948094ba6be5d3a51
MikeTryArtd <mikeleano26@gmail.com>
2026-04-24 17:22:10 +0800

    route for anime detail

--- backend/Routes/Anime.ts
+++ backend/Routes/Anime.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { getAllAnime } from "../Controllers/AllAnimeController";
 import { SearchAnime } from "../Controllers/SearchAnimeController";
 import {TopRatedAnime} from "../Controllers/TopRatedAnimeController";
+import { getDetails } from "../Controllers/DetailAnimeController";
 const router = express.Router();
 
 
 router.get('/api/animeAll', getAllAnime);
 router.get('/api/searchAnime/:query', SearchAnime);
 router.get('/api/topRated', TopRatedAnime );
+router.get("/api/getAnimeDetail/:id", getDetails);
 export default router;
