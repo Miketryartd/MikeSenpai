13e81cc8a4e2b17bd9d70cee271563297f12cdd2
MikeTryArtd <mikeleano26@gmail.com>
2026-04-26 12:09:06 +0800

    updated hook

--- AnimeApp/src/Hooks/useTopRated.ts
+++ AnimeApp/src/Hooks/useTopRated.ts
@@ -1,41 +1,39 @@
-import { useEffect, useState } from "react";
-import type { AniData } from "../Types/Interface";
-import { getTopRated } from "../Services/topRated";
+import { useState, useEffect } from "react";
 
-export function useTopRated() {
-  const [results, setResults] = useState<AniData[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
+const TOTAL_PAGES = 354; 
+
+export function useTopRated(page: number = 1) {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    let isMounted = true;
+    const controller = new AbortController();
 
-    const fetchApi = async () => {
+    const fetchTopRated = async () => {
       setLoading(true);
       setError(null);
 
       try {
-        const data = await getTopRated();
-        if (isMounted) {
-          setResults(data.AniData);
-        }
-      } catch (err: unknown) {
-        if (err instanceof Error) {
-          setError(err.message);
-        } else {
-          setError("Something went wrong");
-        }
-      } finally {
-        if (isMounted) setLoading(false);
+        const res = await fetch(`http://localhost:3000/mikesenpai/api/topRated/${page}`, {
+          signal: controller.signal,
+        });
+
+        const data = await res.json();
+        setResults(data.AniData);
+        setLoading(false);
+
+      } catch (err: any) {
+        if (err.name === "AbortError") return;
+        setError("Failed to load");
+        setLoading(false);
       }
     };
 
-    fetchApi();
+    fetchTopRated();
+    return () => controller.abort();
+  }, [page]);
 
-    return () => {
-      isMounted = false;
-    };
-  }, []);
 
-  return { results, loading, error };
+  return { results, loading, error, totalPages: TOTAL_PAGES };
 }
