c83dc5365708480e2559df1efc2b3d3fc5b0602d
MikeTryArtd <mikeleano26@gmail.com>
2026-04-28 14:18:00 +0800

    forgot to put route

--- backend/server.ts
+++ backend/server.ts
@@ -6,6 +6,7 @@ import userRoutes from "./Routes/UserRoute.js";
 import animeRoutes from "./Routes/Anime.js";
 import mongoose from "mongoose";
 import commentRoutes from "./Routes/CommentRoute.js";
+import pingRoute from "./Routes/Ping.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use('/mikesenpai', animeRoutes);
 app.use('/mikenichan', userRoutes);
 app.use('/mikesempai', commentRoutes);
+app.use('/mikekawai', pingRoute)
 
 const PORT = process.env.HOST || 3000;
 
