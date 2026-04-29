9e98bde323b0fdba5cd8cfc1a84d4c7b2cd54d25
MikeTryArtd <mikeleano26@gmail.com>
2026-04-29 18:30:42 +0800

    added socaial links

--- AnimeApp/src/Components/Nav.tsx
+++ AnimeApp/src/Components/Nav.tsx
@@ -60,7 +60,7 @@ function Nav() {
                   ⟨ Mike Senpai ⟩
                 </span>
                 <span className="text-[10px] text-gray-600 tracking-widest m-5">
-                  Please follow my github
+                O appreciate the follow!
                 </span>
               </Link>
             </div>
@@ -79,12 +79,18 @@ function Nav() {
               </button>
             </form>
 
-            <a href="https://github.com" className="cursor-pointer border border-purple-700 text-purple-400 hover:bg-purple-700 hover:text-white px-3 py-2 rounded-lg transition-colors">
+            <a href="https://https://github.com/Miketryartd/MikeSenpai.com" className="cursor-pointer border border-purple-700 text-purple-400 hover:bg-purple-700 hover:text-white px-3 py-2 rounded-lg transition-colors">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M5.315 2.1c.791 -.113 1.9 .145 3.333 .966l.272 .161l.16 .1l.397 -.083a13.3 13.3 0 0 1 4.59 -.08l.456 .08l.396 .083l.161 -.1c1.385 -.84 2.487 -1.17 3.322 -1.148l.164 .008l.147 .017l.076 .014l.05 .011l.144 .047a1 1 0 0 1 .53 .514a5.2 5.2 0 0 1 .397 2.91l-.047 .267l-.046 .196l.123 .163c.574 .795 .93 1.728 1.03 2.707l.023 .295l.007 .272c0 3.855 -1.659 5.883 -4.644 6.68l-.245 .061l-.132 .029l.014 .161l.008 .157l.004 .365l-.002 .213l-.003 3.834a1 1 0 0 1 -.883 .993l-.117 .007h-6a1 1 0 0 1 -.993 -.883l-.007 -.117v-.734c-1.818 .26 -3.03 -.424 -4.11 -1.878l-.535 -.766c-.28 -.396 -.455 -.579 -.589 -.644l-.048 -.019a1 1 0 0 1 .564 -1.918c.642 .188 1.074 .568 1.57 1.239l.538 .769c.76 1.079 1.36 1.459 2.609 1.191l.001 -.678l-.018 -.168a5.03 5.03 0 0 1 -.021 -.824l.017 -.185l.019 -.12l-.108 -.024c-2.976 -.71 -4.703 -2.573 -4.875 -6.139l-.01 -.31l-.004 -.292a5.6 5.6 0 0 1 .908 -3.051l.152 -.222l.122 -.163l-.045 -.196a5.2 5.2 0 0 1 .145 -2.642l.1 -.282l.106 -.253a1 1 0 0 1 .529 -.514l.144 -.047l.154 -.03z"/>
               </svg>
             </a>
+
+            <a  href="https://www.instagram.com/maiiyykk/"  className="cursor-pointer border border-purple-700 text-purple-400 hover:bg-purple-700 hover:text-white px-3 py-2 rounded-lg transition-colors">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-brand-instagram"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M16 3a5 5 0 0 1 5 5v8a5 5 0 0 1 -5 5h-8a5 5 0 0 1 -5 -5v-8a5 5 0 0 1 5 -5zm-4 5a4 4 0 0 0 -3.995 3.8l-.005 .2a4 4 0 1 0 4 -4m4.5 -1.5a1 1 0 0 0 -.993 .883l-.007 .127a1 1 0 0 0 1.993 .117l.007 -.127a1 1 0 0 0 -1 -1" /></svg>
+            </a>
+
+            
 
             {isLoggedIn ? (
               <p className="border rounded-full bg-gray-600 w-10 h-10 p-2 text-center">{real_name}</p>
